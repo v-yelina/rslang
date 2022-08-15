9257c1fdd11eaf3bdda3f3771bf048bca2aeab04
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-15 19:43:03 +0200

    feat: add header component

---
+++ src/components/shared/header/header.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Header: FC = () => (
+  <>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/textbook">Textbook</Link>
+      <Link to="/sprint">Sprint</Link>
+      <Link to="/audiochallenge">Audiochallenge</Link>
+      <Link to="/statistics">Statistics</Link>
+    </div>
+    <Outlet />
+  </>
+);
+
+export default Header;
