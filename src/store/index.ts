1520a998c51ffd5e45340e8085f7a131e7b82c49
v-yelina <heytridvaodin@gmail.com>
2022-08-16 10:33:44 +0200

    fix: fix typo in rootReducer file name

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './slices/rootReduser';
+import rootReducer from './slices/rootReducer';
 
 export const store = configureStore({
     reducer: {
@@ -9,3 +9,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
