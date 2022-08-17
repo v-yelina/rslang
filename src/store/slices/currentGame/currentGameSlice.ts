55602a40126993d771a5ead190082c54565a1a10
v-yelina <heytridvaodin@gmail.com>
2022-08-17 10:02:59 +0200

    feat: add arrays for wrong and right answers in state

--- src/store/slices/currentGame/currentGameSlice.ts
+++ src/store/slices/currentGame/currentGameSlice.ts
@@ -5,20 +5,24 @@ type wordsSourceType = 'group' | 'textbook' | undefined;
 
 type gameType = 'audiochallenge' | 'sprint' | undefined;
 
-interface WordToTrain extends Pick<IWord, 'id' | 'word' | 'image' | 'audio' | 'wordTranslate'> {
-  answer: string;
-}
+type WordToTrain = Pick<IWord, 'id' | 'word' | 'image' | 'audio' | 'wordTranslate'>;
+type Answer = { answer: string; word: string };
+type RightAnswer = Pick<Answer, 'word'>;
 
 export type CurrentGameState = {
   wordsSource: wordsSourceType;
   gameType: gameType;
   words: WordToTrain[];
+  rightAnswers: RightAnswer[];
+  wrongAnswers: Answer[];
 };
 
 const initialState: CurrentGameState = {
   wordsSource: undefined,
   gameType: undefined,
   words: [],
+  rightAnswers: [],
+  wrongAnswers: [],
 };
 
 export const currentGameSlice = createSlice({
@@ -34,14 +38,18 @@ export const currentGameSlice = createSlice({
     setWordsToTrain: (state, action: PayloadAction<WordToTrain[]>) => {
       state.words = action.payload;
     },
+    addRightAnswer: (state, action: PayloadAction<RightAnswer>) => {
+      state.rightAnswers.push(action.payload);
+    },
+    addWrongAnswer: (state, action: PayloadAction<Answer>) => {
+      state.wrongAnswers.push(action.payload);
+    },
 
     clearCurrentgame: () => initialState,
   },
   extraReducers: () => {},
 });
 
-export const {
-  setGameType, setWordsSource, setWordsToTrain, clearCurrentgame,
-} = currentGameSlice.actions;
+export const { setGameType, setWordsSource, setWordsToTrain, clearCurrentgame } = currentGameSlice.actions;
 
 export default currentGameSlice.reducer;
