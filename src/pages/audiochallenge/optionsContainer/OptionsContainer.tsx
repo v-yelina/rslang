997794db27c0760b3973c87ba11c4fad77303111
v-yelina <heytridvaodin@gmail.com>
2022-08-17 22:55:33 +0200

    feat: add doesn't know button

--- src/pages/audiochallenge/optionsContainer/OptionsContainer.tsx
+++ src/pages/audiochallenge/optionsContainer/OptionsContainer.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Button } from 'antd';
 import AnswerOption from '../answerOption';
 
 const OptionsContainer: FC = () => (
@@ -8,8 +9,8 @@ const OptionsContainer: FC = () => (
     <AnswerOption num="3" />
     <AnswerOption num="4" />
     <AnswerOption num="5" />
+    <Button type="dashed">Don&lsquo;t know</Button>
   </div>
-
 );
 
 export default OptionsContainer;
