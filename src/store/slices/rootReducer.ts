a082e529dd6f357172620812c0d8621345f6fa58
v-yelina <heytridvaodin@gmail.com>
2022-08-17 13:34:18 +0200

    refactor: add shorter reducer variable name

--- src/store/slices/rootReducer.ts
+++ src/store/slices/rootReducer.ts
@@ -1,4 +1,4 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import currentGameSlice from './currentGame';
 
-export default combineReducers({ currentGameSlice });
+export default combineReducers({ currentGame: currentGameSlice });
