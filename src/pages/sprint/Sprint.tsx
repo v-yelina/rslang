1537ad9d68b8980462d9ea0186a6e2de385570eb
Eremor <artem.s13@gmail.com>
2022-08-18 15:05:23 +0300

    feat: add game leave button

--- src/pages/sprint/Sprint.tsx
+++ src/pages/sprint/Sprint.tsx
@@ -1,13 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import LeaveGameButton from '../../components/shared/button/leave-game-button';
+import ConfirmModal from '../../components/shared/modal/confirm-modal';
 import Timer from '../../components/timer';
+import './sprint.scss';
 
-const Sprint: FC = () => (
-  <section className="sprint">
-    <div className="sprint__header">
-      <Timer />
-    </div>
-    <div className="sprint__field" />
-  </section>
-);
+const Sprint: FC = () => {
+  const [isVisibleLeaveModal, setVisibleLeaveModal] = useState(false);
+
+  return (
+    <section className="sprint">
+      <div className="sprint__header">
+        <Timer />
+        <LeaveGameButton setVisible={setVisibleLeaveModal} />
+      </div>
+      <div className="sprint__field" />
+      {
+        isVisibleLeaveModal
+        && (
+        <ConfirmModal
+          isVisible={isVisibleLeaveModal}
+          setVisible={setVisibleLeaveModal}
+        />
+        )
+      }
+    </section>
+  );
+};
 
 export default Sprint;
