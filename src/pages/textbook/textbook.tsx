20d7925b3f6d5ca44ac70c1738cc9d29be21bed7
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-19 18:28:40 +0200

    feat: add fetching words for words-list

--- src/pages/textbook/textbook.tsx
+++ src/pages/textbook/textbook.tsx
@@ -1,29 +1,31 @@
-import React, { FC } from 'react';
-import { IWord } from '../../interfaces/IWord';
+import React, { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { fetchWordsByGroupAndPage } from '../../store/thunks';
 import WordCard from './components/word-card';
 
-const wordData = {
-  id: '5e9f5ee35eb9e72bc21af4a0',
-  group: 0,
-  page: 0,
-  word: 'alcohol',
-  image: 'files/01_0002.jpg',
-  audio: 'files/01_0002.mp3',
-  audioMeaning: 'files/01_0002_meaning.mp3',
-  audioExample: 'files/01_0002_example.mp3',
-  textMeaning: '<i>Alcohol</i> is a type of drink that can make people drunk.',
-  textExample: 'A person should not drive a car after he or she has been drinking <b>alcohol</b>.',
-  transcription: '[ǽlkəhɔ̀ːl]',
-  textExampleTranslate: 'Человек не должен водить машину после того, как он выпил алкоголь',
-  textMeaningTranslate: 'Алкоголь - это тип напитка, который может сделать людей пьяными',
-  wordTranslate: 'алкоголь',
-};
+const Textbook: FC = () => {
+  const tempPage = '1';
+  const tempGroup = '1';
+  const dispatch = useAppDispatch();
+  const { isLoading, currentWords } = useAppSelector((state) => state.textbook);
 
-const Textbook: FC = () => (
-  <>
-    <h2>Textbook Page</h2>
-    <WordCard wordData={wordData as IWord} />
-  </>
-);
+  useEffect(() => {
+    dispatch(fetchWordsByGroupAndPage({ group: tempGroup, page: tempPage }));
+  }, []);
+
+  return (
+    <>
+      <h2>Textbook Page</h2>
+      {isLoading && <h3>Loading words...</h3>}
+      {currentWords.length && (
+        <div className="words-list">
+          {currentWords.map((word) => (
+            <WordCard key={word.id} wordData={word} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Textbook;
--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -2,12 +2,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import ENV from '../config/config';
 import { PageData } from './types';
 
+// eslint-disable-next-line
 export const fetchWordsByGroupAndPage = createAsyncThunk(
   'textbook/fetchWords',
   async (pageData: PageData, thunkAPI) => {
     try {
       const response = await fetch(
-        `${ENV.BASE_URL}words?group=${pageData.group}&page=${pageData.page}`
+        `${ENV.BASE_URL}words?group=${pageData.group}&page=${pageData.page}`,
       );
 
       if (!response.ok) {
@@ -19,5 +20,5 @@ export const fetchWordsByGroupAndPage = createAsyncThunk(
     } catch (error) {
       return thunkAPI.rejectWithValue((error as Error).message);
     }
-  }
+  },
 );
