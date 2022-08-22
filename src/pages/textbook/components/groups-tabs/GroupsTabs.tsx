4d738d6da6833ef5a64042d7db977c6c5984d632
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-22 19:58:08 +0200

    fix: save searchParams when switching group tab

--- src/pages/textbook/components/groups-tabs/GroupsTabs.tsx
+++ src/pages/textbook/components/groups-tabs/GroupsTabs.tsx
@@ -1,25 +1,34 @@
 import React, { FC } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Tabs } from 'antd';
-
-import { wordsGroups } from '../../../../constants';
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
+import { wordsGroups } from '../../../../constants';
+import { setCurrentPageData, selectCurrentGroup } from '../../../../store/slices/textbook';
+import { preparePageData, SEARCH_INITIAL_PAGE } from '../../helpers';
 
 import './groups-tabs.scss';
-import { setCurrentGroup } from '../../../../store/slices/textbook';
 
 const { TabPane } = Tabs;
 
 const GroupsTabs: FC = () => {
+  // eslint-disable-next-line
+  const [_, setParams] = useSearchParams();
   const dispatch = useAppDispatch();
-  const { currentGroup } = useAppSelector((state) => state.textbook);
+  const group = useAppSelector(selectCurrentGroup);
+
   const onGroupChange = (groupKey: string) => {
-    dispatch(setCurrentGroup(Number(groupKey)));
+    setParams({ group: groupKey, page: SEARCH_INITIAL_PAGE });
+    const newPageData = {
+      page: preparePageData(SEARCH_INITIAL_PAGE),
+      group: preparePageData(groupKey),
+    };
+    dispatch(setCurrentPageData(newPageData));
   };
 
   return (
-    <Tabs defaultActiveKey={currentGroup.toString()} onChange={onGroupChange}>
+    <Tabs defaultActiveKey={(Number(group) + 1).toString()} onChange={onGroupChange}>
       {[...wordsGroups].map((groupIndex) => (
-        <TabPane key={groupIndex} tab={`Group ${groupIndex + 1}`} />
+        <TabPane key={groupIndex} tab={`Group ${groupIndex}`} />
       ))}
     </Tabs>
   );
