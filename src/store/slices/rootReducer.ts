30850dc3bc4f9cdcdf95d75bab7e3916069a20ad
v-yelina <heytridvaodin@gmail.com>
2022-08-22 23:19:22 +0200

    feat: add auth slice to root reducer

--- src/store/slices/rootReducer.ts
+++ src/store/slices/rootReducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import authSlice from './auth';
 import currentGameSlice from './currentGame';
 import textbookSlice from './textbook';
 
-export default combineReducers({ currentGame: currentGameSlice, textbook: textbookSlice });
+export default combineReducers({ auth: authSlice, currentGame: currentGameSlice, textbook: textbookSlice });
