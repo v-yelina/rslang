f27e38b2a45995dd09a3c2309d5e2bd23124d4ca
v-yelina <heytridvaodin@gmail.com>
2022-08-22 13:35:49 +0200

    feat: add login form

---
+++ src/pages/login/loginForm/LoginForm.tsx
@@ -0,0 +1,47 @@
+import { Button, Form, Input } from 'antd';
+import React, { FC } from 'react';
+
+const LoginForm: FC = () => {
+  const onFinish = (values: string) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = () => {
+    console.log('Failed.');
+  };
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 5 }}
+      wrapperCol={{ span: 10 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
