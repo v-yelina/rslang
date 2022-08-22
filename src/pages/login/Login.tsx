b27da5a7a42e2fbfbaefafed2b6cefde77e8e5c7
v-yelina <heytridvaodin@gmail.com>
2022-08-22 13:36:21 +0200

    feat: add login page

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import LoginForm from './loginForm';
+
+const Login: FC = () => (
+  <>
+    <h2>Login Page</h2>
+    ;
+    <LoginForm />
+  </>
+);
+
+export default Login;
