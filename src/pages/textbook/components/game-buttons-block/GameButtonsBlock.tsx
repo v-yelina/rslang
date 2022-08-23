7c6447cac458e43832d66f569fdc6a829d0cf0fd
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-23 21:21:40 +0200

    refactor: delete unnecessary key

--- src/pages/textbook/components/game-buttons-block/GameButtonsBlock.tsx
+++ src/pages/textbook/components/game-buttons-block/GameButtonsBlock.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
-import { GAME_TYPE } from '../../../../constants';
 import GameButton from '../game-button/GameButton';
 
 import './game-buttons-block.scss';
 
 const GameButtonsBlock: FC = () => (
   <div className="game-buttons-wrapper">
-    <GameButton game="sprint" key={GAME_TYPE.SPRINT} />
-    <GameButton game="audiochallenge" key={GAME_TYPE.AUDIOCHALLENGE} />
+    <GameButton game="sprint" />
+    <GameButton game="audiochallenge" />
   </div>
 );
 
