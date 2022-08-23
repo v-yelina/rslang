f211e1a2c55f9c0ee7ef28c6fb8ecaccb01ec956
v-yelina <heytridvaodin@gmail.com>
2022-08-23 09:47:52 +0200

    feat: create login and registration thunks

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import ENV from '../config/config';
+import { ILogin } from '../interfaces/ILogin';
+import { IUser } from '../interfaces/IUser';
 import { PageData } from './types';
 
 // eslint-disable-next-line
@@ -17,6 +19,52 @@ export const fetchWordsByGroupAndPage = createAsyncThunk(
 
       const words = await response.json();
       return words;
+    } catch (error) {
+      return thunkAPI.rejectWithValue((error as Error).message);
+    }
+  },
+);
+
+export const registration = createAsyncThunk(
+  'user/register',
+  async (userData: IUser, thunkAPI) => {
+    try {
+      const response = await fetch(`${ENV.BASE_URL}users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        throw new Error('User can\'t be created, please try again');
+      }
+      const authInfo = await response.json();
+      return authInfo;
+    } catch (error) {
+      return thunkAPI.rejectWithValue((error as Error).message);
+    }
+  },
+);
+
+export const login = createAsyncThunk(
+  'user/login',
+  async (loginData: ILogin, thunkAPI) => {
+    try {
+      const response = await fetch(`${ENV.BASE_URL}signin`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed, please try again');
+      }
+      const loginInfo = await response.json();
+      return loginInfo;
     } catch (error) {
       return thunkAPI.rejectWithValue((error as Error).message);
     }
