1251520f056fd0a34b29f0438cc5ca96727cc59f
v-yelina <heytridvaodin@gmail.com>
2022-08-23 23:12:23 +0200

    feat: clean state after changing tab

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,21 +1,29 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Button } from 'antd';
 import LoginForm from './loginForm';
 import RegistrationForm from './registrationForm';
 import './login.scss';
+import { useAppDispatch } from '../../store/hooks';
+import { clearError, clearIsRegistred } from '../../store/slices/auth';
 
 const Login: FC = () => {
-  const [isRegistred, setIsRegistred] = useState(true);
+  const dispatch = useAppDispatch();
+  const [loginTab, setLoginTab] = useState(true);
+
+  useEffect(() => {
+    dispatch(clearIsRegistred());
+    dispatch(clearError());
+  }, [loginTab]);
 
   return (
     <>
       <h2>Login Page</h2>
       <div className="login-forms">
         <div className="login-signin-buttons">
-          <Button type="dashed" onClick={() => { setIsRegistred(true); }}>Login</Button>
-          <Button type="dashed" onClick={() => { setIsRegistred(false); }}>Registration</Button>
+          <Button type="dashed" onClick={() => { setLoginTab(true); }}>Login</Button>
+          <Button type="dashed" onClick={() => { setLoginTab(false); }}>Registration</Button>
         </div>
-        {isRegistred ? <LoginForm /> : <RegistrationForm />}
+        {loginTab ? <LoginForm /> : <RegistrationForm />}
       </div>
     </>
   );
