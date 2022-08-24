40107a28b401dbc2e44c4413ff4e81ed030b4845
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-24 13:13:55 +0200

    refactor: thunks for textbook and sprint based on api calls

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -3,87 +3,69 @@ import ENV from '../config/config';
 import { ILogin } from '../interfaces/ILogin';
 import { IUser } from '../interfaces/IUser';
 import { IWord } from '../interfaces/IWord';
+import { fetchWordsByGroupAndPage } from '../utils/api/api';
 import { PageData } from './types';
 
-export const fetchWordsByGroupAndPage = createAsyncThunk(
+export const fetchWordsForTextbook = createAsyncThunk(
   'textbook/fetchWords',
-  async (pageData: PageData, thunkAPI) => {
+  async (pageData: PageData, { rejectWithValue }) => {
+    const { group, page } = pageData;
     try {
-      const response = await fetch(
-        `${ENV.BASE_URL}words?group=${pageData.group}&page=${pageData.page}`,
-      );
-
-      if (!response.ok) {
-        throw new Error('Words are not found');
-      }
-
-      const words = await response.json();
+      const words = await fetchWordsByGroupAndPage(group, page);
       return words;
     } catch (error) {
-      return thunkAPI.rejectWithValue((error as Error).message);
+      return rejectWithValue((error as Error).message);
     }
   },
 );
 
-export const registration = createAsyncThunk(
-  'user/register',
-  async (userData: IUser, thunkAPI) => {
-    try {
-      const response = await fetch(`${ENV.BASE_URL}users`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      });
+export const registration = createAsyncThunk('user/register', async (userData: IUser, thunkAPI) => {
+  try {
+    const response = await fetch(`${ENV.BASE_URL}users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
 
-      if (!response.ok) {
-        throw new Error(await response.text());
-      }
-      const authInfo = await response.json();
-      return authInfo;
-    } catch (error) {
-      return thunkAPI.rejectWithValue((error as Error).message);
+    if (!response.ok) {
+      throw new Error(await response.text());
     }
-  },
-);
+    const authInfo = await response.json();
+    return authInfo;
+  } catch (error) {
+    return thunkAPI.rejectWithValue((error as Error).message);
+  }
+});
 
-export const login = createAsyncThunk(
-  'user/login',
-  async (loginData: ILogin, thunkAPI) => {
-    try {
-      const response = await fetch(`${ENV.BASE_URL}signin`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData),
-      });
+export const login = createAsyncThunk('user/login', async (loginData: ILogin, thunkAPI) => {
+  try {
+    const response = await fetch(`${ENV.BASE_URL}signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+    });
 
-      if (!response.ok) {
-        throw new Error('Login failed, please try again');
-      }
-      const loginInfo = await response.json();
-      return loginInfo;
-    } catch (error) {
-      return thunkAPI.rejectWithValue((error as Error).message);
+    if (!response.ok) {
+      throw new Error('Login failed, please try again');
     }
-  },
-);
+    const loginInfo = await response.json();
+    return loginInfo;
+  } catch (error) {
+    return thunkAPI.rejectWithValue((error as Error).message);
+  }
+});
 
 export const fetchWordsToSprintGame = createAsyncThunk(
   'sprintGame/fetchWords',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await fetch(`${ENV.BASE_URL}words?group=0&page=0`);
+      const words = await fetchWordsByGroupAndPage('0', '0');
 
-      if (!response.ok) {
-        throw new Error('Words are not found, Server error!');
-      }
-
-      const words: IWord[] = await response.json();
-
-      return words.map((item) => ({
+      return words.map((item: IWord) => ({
         id: item.id,
         word: item.word,
         wordTranslate: item.wordTranslate,
