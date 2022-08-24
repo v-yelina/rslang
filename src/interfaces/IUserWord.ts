3bde02fac21fba0d1ae1437c7775ab93ff03d780
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-24 21:51:06 +0200

    fix: correct IUserWord interface

--- src/interfaces/IUserWord.ts
+++ src/interfaces/IUserWord.ts
@@ -3,8 +3,8 @@ import { DifficultyType } from '../store/types';
 export interface IUserWord {
   difficulty: DifficultyType;
   optional: {
-    wordId: string;
     isLearned: boolean;
     rightAnswersCounter: number;
   };
+  wordId?: string;
 }
