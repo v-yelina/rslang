f3edde1add3fe585cfd2c4a6bc6c36dc1f2d4f6c
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-24 13:12:09 +0200

    feat: add endpoints for words and users

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,13 @@
 interface IENV {
   BASE_URL?: string;
+  WORDS_URL: string;
+  USERS_URL: string;
 }
 
 const ENV: IENV = {
   BASE_URL: process.env.REACT_APP_BASE_URL,
+  WORDS_URL: `${process.env.REACT_APP_BASE_URL}words`,
+  USERS_URL: `${process.env.REACT_APP_BASE_URL}users`,
 };
 
 export default ENV;
