e95e7bbd35793c1e534876ca75c0efe04e5107ca
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-08-24 13:57:05 +0200

    feat: add api calls for userWords

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,4 +1,5 @@
 import ENV from '../../config/config';
+import { IUserWord } from '../../interfaces/IUserWord';
 import { IWord } from '../../interfaces/IWord';
 
 // Words
@@ -25,4 +26,104 @@ export const fetchWordById = async (wordId: string) => {
   const wordData: IWord = await response.json();
 
   return wordData;
+};
+
+// UserWords
+
+export const fetchUserWords = async (userId: string, userToken: string) => {
+  const response = await fetch(`${ENV.USERS_URL}/${userId}/words`, {
+    method: 'GET',
+    headers: {
+      Authorisation: `Bearer ${userToken}`,
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('User words not found');
+  }
+
+  const userWords: IUserWord[] = await response.json();
+
+  return userWords;
+};
+
+export const createUserWord = async (
+  userId: string,
+  userToken: string,
+  wordId: string,
+  userWord: IUserWord,
+) => {
+  const response = await fetch(`${ENV.USERS_URL}/${userId}/words/${wordId}`, {
+    method: 'POST',
+    headers: {
+      Authorisation: `Bearer ${userToken}`,
+      accept: 'application/json',
+    },
+    body: JSON.stringify(userWord),
+  });
+
+  if (!response.ok || response.status !== 200) {
+    throw new Error('User word not created');
+  }
+
+  const createdWord: IUserWord = await response.json();
+
+  return createdWord;
+};
+
+export const fetchUserWordById = async (userId: string, userToken: string, wordId: string) => {
+  const response = await fetch(`${ENV.USERS_URL}/${userId}/words/${wordId}`, {
+    method: 'GET',
+    headers: {
+      Authorisation: `Bearer ${userToken}`,
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.ok || response.status !== 200) {
+    throw new Error('User word not found');
+  }
+
+  const userWord: IUserWord = await response.json();
+
+  return userWord;
+};
+
+export const updateUserWord = async (
+  userId: string,
+  userToken: string,
+  wordId: string,
+  userWord: IUserWord,
+) => {
+  const response = await fetch(`${ENV.USERS_URL}/${userId}/words/${wordId}`, {
+    method: 'PUT',
+    headers: {
+      Authorisation: `Bearer ${userToken}`,
+      accept: 'application/json',
+    },
+    body: JSON.stringify(userWord),
+  });
+
+  if (!response.ok || response.status !== 200) {
+    throw new Error('User word not updated');
+  }
+
+  const updatedWord: IUserWord = await response.json();
+
+  return updatedWord;
+};
+
+export const deleteUserWord = async (userId: string, userToken: string, wordId: string) => {
+  const response = await fetch(`${ENV.USERS_URL}/${userId}/words/${wordId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorisation: `Bearer ${userToken}`,
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.ok || response.status !== 204) {
+    throw new Error('User word not deleted');
+  }
 };
