6d6d33c6d362f0bf3f397d1eb04dea251039f1bb
v-yelina <heytridvaodin@gmail.com>
2022-08-25 14:35:30 +0200

    fix: clean state after log out

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
-import { useAppSelector } from '../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { clearAuth } from '../../../store/slices/auth';
 
 const Header: FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { isLogged } = useAppSelector((state) => state.auth);
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    dispatch(clearAuth());
+    navigate('/');
+  };
 
   return (
     <>
@@ -16,7 +25,7 @@ const Header: FC = () => {
         <Link to="/statistics">Statistics</Link>
         <Link to="/demo">Demo</Link>
         <Link to="/login">Login</Link>
-        {isLogged && <Button type="primary" onClick={() => { localStorage.removeItem('user'); }}>Log out</Button>}
+        {isLogged && <Button type="primary" onClick={logout}>Log out</Button>}
       </div>
       <Outlet />
     </>
