66d6921a0ec42bad224a51275bfe4f59fe26b2c7
v-yelina <heytridvaodin@gmail.com>
2022-08-29 13:56:03 +0200

    feat: add carousel for mini-games statistic

--- src/pages/statistics/statistics.tsx
+++ src/pages/statistics/statistics.tsx
@@ -1,5 +1,22 @@
 import React, { FC } from 'react';
+import {
+  Carousel,
+} from 'antd';
+import GameStatistic from './gameStatistic';
 
-const Statistics: FC = () => <h2>Statistics Page</h2>;
+const Statistics: FC = () => (
+  <>
+    <h2>Statistics</h2>
+    <Carousel>
+      <div>
+        <GameStatistic gameName="Audiochallenge" />
+      </div>
+      <div>
+        <GameStatistic gameName="Sprint" />
+      </div>
+    </Carousel>
+
+  </>
+);
 
 export default Statistics;
