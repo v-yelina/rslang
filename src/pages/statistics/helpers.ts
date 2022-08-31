b58ea33786a296b6b9e7a386e6f03bb2ced488e5
v-yelina <heytridvaodin@gmail.com>
2022-08-31 22:23:36 +0200

    feat: add update word function

--- src/pages/statistics/helpers.ts
+++ src/pages/statistics/helpers.ts
@@ -1,3 +1,4 @@
+import { IUserWord } from "../../interfaces/IUserWord";
 import { gameType } from "../../store/slices/currentGame";
 import { addAnswersToSliceArgs } from "../audiochallenge/Audiochallenge";
 
@@ -5,29 +6,32 @@ export const checkDate = (statDate:string, today: string) => {
   return statDate === today;
 }
 
-export const updateWord = (data: addAnswersToSliceArgs, game: gameType) => {
+export const updateWord = (data: addAnswersToSliceArgs, game: gameType):IUserWord => {
+  const gamesStat = updateGamesStat(game, data);
   const newRightAnswersCounter = data.answer.optional.rightAnswersCounter + 1;
   const isLearned = checkIfLearned(newRightAnswersCounter, data.answer.difficulty === 'difficult');
-
+  return {difficulty:data.answer.difficulty, optional: {isLearned, isNew: false, rightAnswersCounter:newRightAnswersCounter, ...gamesStat}}
 }
 
 const updateGamesStat = (gameType:gameType, data: addAnswersToSliceArgs) => {
+  let gamesStat = {sprint: data.answer.optional.sprint, audiochallenge: data.answer.optional.audiochallenge};
   if (gameType === 'audiochallenge' && data.isRight) {
-    return {sprint: {...data.answer.optional.sprint}, audiochallenge: {rightCounter: data.answer.optional.audiochallenge.rightCounter + 1,
+    gamesStat = {sprint: {...data.answer.optional.sprint}, audiochallenge: {rightCounter: data.answer.optional.audiochallenge.rightCounter + 1,
       wrongCounter: data.answer.optional.audiochallenge.wrongCounter}}
   }
   if (gameType === 'audiochallenge' && !data.isRight) {
-    return {sprint: {...data.answer.optional.sprint}, audiochallenge: {rightCounter: data.answer.optional.audiochallenge.rightCounter,
+    gamesStat = {sprint: {...data.answer.optional.sprint}, audiochallenge: {rightCounter: data.answer.optional.audiochallenge.rightCounter,
       wrongCounter: data.answer.optional.audiochallenge.wrongCounter + 1}}
   }
   if (gameType === 'sprint' && data.isRight) {
-    return {sprint: {rightCounter: data.answer.optional.sprint.rightCounter + 1,
+    gamesStat = {sprint: {rightCounter: data.answer.optional.sprint.rightCounter + 1,
       wrongCounter: data.answer.optional.sprint.wrongCounter}, audiochallenge: {...data.answer.optional.audiochallenge}}
   }
   if (gameType === 'sprint' && !data.isRight) {
-    return {sprint: {rightCounter: data.answer.optional.sprint.rightCounter,
+    gamesStat = {sprint: {rightCounter: data.answer.optional.sprint.rightCounter,
       wrongCounter: data.answer.optional.sprint.wrongCounter + 1}, audiochallenge: {...data.answer.optional.audiochallenge}}
   }
+  return gamesStat;
 }
 
 const checkIfLearned = (counter: number, isDifficult: boolean) => {
