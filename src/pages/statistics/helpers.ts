266cddd1d874edb48749845c84f949b1967e05fb
v-yelina <heytridvaodin@gmail.com>
2022-08-31 19:29:38 +0200

    feat: add check if word is learned after game

--- src/pages/statistics/helpers.ts
+++ src/pages/statistics/helpers.ts
@@ -1,3 +1,20 @@
+import { gameType } from "../../store/slices/currentGame";
+import { addAnswersToSliceArgs } from "../audiochallenge/Audiochallenge";
+
 export const checkDate = (statDate:string, today: string) => {
   return statDate === today;
+}
+
+export const updateWord = (data: addAnswersToSliceArgs, game: gameType) => {
+
+}
+
+const checkIfLearned = (counter: number, isDifficult: boolean) => {
+  if (counter >= 3 && !isDifficult) {
+    return true;
+  } 
+  if (counter >= 5 && isDifficult) {
+    return true;
+  }
+  return false;
 }
