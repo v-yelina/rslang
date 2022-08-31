df3d3e02f06c34869f050c65335f45853b2ffffa
v-yelina <heytridvaodin@gmail.com>
2022-08-31 09:37:24 +0200

    feat: add extra reducers to statistic slice

--- src/store/slices/statistic/statisticSlice.ts
+++ src/store/slices/statistic/statisticSlice.ts
@@ -1,18 +1,22 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ISettings } from '../../../interfaces/ISettings';
 import { IStatistic } from '../../../interfaces/IStatistic';
-import { fetchUserStatistic } from '../../thunks';
+import { fetchUserSettings, fetchUserStatistic, updateSettings, updateStatistic } from '../../thunks';
 
 type StatisticState = {
   isLoading: boolean;
   statistic: IStatistic;
+  settings: ISettings;
+  error: string | null,
 }
 
 const initialState: StatisticState = {
   isLoading: false,
+  error: null,
   statistic: {
     learnedWords: 0,
     optional: {
-      statisticDay: new Date(),
+      statisticDay: '',
       audiochallenge: {
         newWords: 0,
         correctAnswers: 0,
@@ -27,6 +31,14 @@ const initialState: StatisticState = {
         longestCombo: 0,
         gamesPlayed: 0,
       },
+    },
+  },
+  settings: {
+    wordsPerDay: 0,
+    optional: {
+      day: '',
+      newWordsCount: 0,
+      gamesCount: 0,
     },
   },
 
@@ -45,6 +57,35 @@ export const statisticSlice = createSlice({
     [fetchUserStatistic.fulfilled.type]: (state, action: PayloadAction<IStatistic>) => {
       state.isLoading = false;
       state.statistic = {
+        ...action.payload,
+      };
+    },
+    [fetchUserStatistic.rejected.type]: (state, action: PayloadAction<string>) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [updateStatistic.fulfilled.type]: (state, action: PayloadAction<IStatistic>) => {
+      state.isLoading = false;
+      state.statistic = {
+        ...action.payload,
+      };
+    },
+    [fetchUserSettings.pending.type]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchUserSettings.fulfilled.type]: (state, action: PayloadAction<ISettings>) => {
+      state.isLoading = false;
+      state.settings = {
+        ...action.payload,
+      };
+    },
+    [fetchUserSettings.rejected.type]: (state, action: PayloadAction<string>) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [updateSettings.fulfilled.type]: (state, action: PayloadAction<ISettings>) => {
+      state.isLoading = false;
+      state.settings = {
         ...action.payload,
       };
     },
