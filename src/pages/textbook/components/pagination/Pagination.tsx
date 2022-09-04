625c68e80a57d9c1bd9df9f048538f5a03298c24
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-09-05 00:32:13 +0200

    feat: add custom pagination

--- src/pages/textbook/components/pagination/Pagination.tsx
+++ src/pages/textbook/components/pagination/Pagination.tsx
@@ -1,46 +1,106 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { Button, Input } from 'antd';
+import ButtonGroup from 'antd/lib/button/button-group';
 import { useSearchParams } from 'react-router-dom';
-import type { PaginationProps } from 'antd';
-import { Pagination } from 'antd';
+import { WORDS_PER_GROUP, WORDS_PER_PAGE } from '../../../../constants';
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
 import {
   selectCurrentGroup,
   selectCurrentPage,
   setCurrentPageData,
 } from '../../../../store/slices/textbook';
-import { WORDS_PER_GROUP, WORDS_PER_PAGE } from '../../../../constants';
 import { formatPageDataForSlice, formatPageDataForUI } from '../../helpers';
 
 import './pagination.scss';
+
+type PageType = 'current' | 'prev' | 'next' | 'first' | 'last';
 
 const PaginationBlock: FC = () => {
   // eslint-disable-next-line
   const [_, setSearchParams] = useSearchParams();
   const dispatch = useAppDispatch();
+
   const currentGroup = useAppSelector(selectCurrentGroup);
   const currentPage = useAppSelector(selectCurrentPage);
   const pageNumUI = Number(currentPage) + 1;
+  const lastPage = WORDS_PER_GROUP / WORDS_PER_PAGE;
 
-  const onPageChange: PaginationProps['onChange'] = (page: number) => {
+  const [pageSearchValue, setPageSearchValue] = useState('');
+
+  const onPageChange = (newPage: number): void => {
     setSearchParams({
       group: formatPageDataForUI(currentGroup),
-      page: page.toString(),
+      page: newPage.toString(),
     });
     const newPageData = {
-      page: formatPageDataForSlice(page.toString()),
+      page: formatPageDataForSlice(newPage.toString()),
       group: currentGroup,
     };
     dispatch(setCurrentPageData(newPageData));
   };
 
+  const handlePageSearch = (): void => {
+    let newPage: number;
+    if (!Number(pageSearchValue) || Number(pageSearchValue) < 1 || Number(pageSearchValue) > 30) {
+      newPage = 1;
+    } else {
+      newPage = Number(pageSearchValue);
+    }
+    onPageChange(newPage);
+  };
+
+  const handlePageClick = (pageBtnType: PageType): void => {
+    let newPage: number;
+
+    switch (pageBtnType) {
+      case 'first':
+        newPage = 1;
+        break;
+      case 'last':
+        newPage = lastPage;
+        break;
+      case 'prev':
+        newPage = pageNumUI - 1;
+        break;
+      case 'next':
+        newPage = pageNumUI + 1;
+        break;
+      default:
+        newPage = pageNumUI;
+        return;
+    }
+
+    onPageChange(newPage);
+  };
+
   return (
-    <Pagination
-      current={pageNumUI}
-      onChange={onPageChange}
-      pageSize={WORDS_PER_PAGE}
-      total={WORDS_PER_GROUP}
-      showSizeChanger={false}
-    />
+    <div className="textbook-pagination-wrapper">
+      <ButtonGroup className="textbook-pagination">
+        <Button disabled={pageNumUI === 1} onClick={() => handlePageClick('first')}>
+          {'<<'}
+        </Button>
+        <Button disabled={pageNumUI === 1} onClick={() => handlePageClick('prev')}>
+          {'<'}
+        </Button>
+        <Button onClick={() => handlePageClick('current')}>{pageNumUI}</Button>
+        <Button disabled={pageNumUI === lastPage} onClick={() => handlePageClick('next')}>
+          {'>'}
+        </Button>
+        <Button disabled={pageNumUI === lastPage} onClick={() => handlePageClick('last')}>
+          {'>>'}
+        </Button>
+      </ButtonGroup>
+      <div className="pagination-jump">
+        <span>GO TO PAGE: </span>
+        <Input
+          defaultValue=""
+          value={pageSearchValue}
+          onChange={(e) => setPageSearchValue(e.target.value)}
+          onPressEnter={handlePageSearch}
+          style={{ width: '50px' }}
+        />
+      </div>
+    </div>
   );
 };
 
