26b72a0e7014b610b8b4bf0b31607ef726816604
Eremor <artem.s13@gmail.com>
2022-09-04 18:01:17 +0300

    feat: add statistics collection to sprint game

--- src/pages/sprint/Sprint.tsx
+++ src/pages/sprint/Sprint.tsx
@@ -1,4 +1,6 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  FC, useEffect, useRef, useState,
+} from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { clearSprintState, setCurrentWord } from '../../store/slices/sprintGame';
@@ -9,6 +11,20 @@ import { Answer } from '../../store/types';
 import { getRandomTranslate } from './sprintGame';
 import LeaveGameButton from '../../components/shared/button/leave-game-button';
 import ConfirmModal from '../../components/shared/modal/confirm-modal';
+import {
+  createUserWordFromTextbook,
+  fetchUserSettings,
+  fetchUserStatistic,
+  updateSettings,
+  updateStatistic,
+  updateUserWordFromTextbook,
+} from '../../store/thunks';
+import { IDayStat, ISettings } from '../../interfaces/ISettings';
+import {
+  checkDate, countNewWords, getNewDayStat, getToday, updateWord,
+} from '../statistics/helpers';
+import { initialState } from '../../store/slices/statistic';
+import { IStatistic } from '../../interfaces/IStatistic';
 
 import './sprint.scss';
 
@@ -22,14 +38,133 @@ const Sprint: FC = () => {
   const {
     words,
     randomWords,
+    maxCombo,
   } = useAppSelector((state) => state.currentGame);
+  const {
+    statistic,
+    settings,
+  } = useAppSelector((state) => state.statistic);
+  const {
+    user,
+    isLogged,
+  } = useAppSelector((state) => state.auth);
 
   const [isGameFinished, setGameFinished] = useState(false);
   const [gameTime, setGameTime] = useState(DURATION_GAME_SPRINT);
   const [isVisibleLeaveModal, setVisibleLeaveModal] = useState(false);
+  const learned = useRef(0);
+
+  const sendDayStatistic = (newDayStat: IDayStat) => {
+    const newDate: string = statistic.optional.statisticDay;
+    const newStat: IDayStat = {
+      newWordsCount: newDayStat.newWordsCount,
+      gamesCount: newDayStat.gamesCount,
+    };
+    const newOptional = {};
+    Object.defineProperty(newOptional, newDate, {
+      value: { ...newStat },
+      writable: true,
+      enumerable: true,
+    });
+    Object.assign(newOptional, settings.optional);
+    const newSettings: ISettings = {
+      wordsPerDay: settings.wordsPerDay,
+      optional: newOptional,
+    };
+
+    dispatch(updateSettings({
+      userId: user.userId,
+      token: user.token,
+      settings: newSettings,
+    }));
+  };
+
+  const updateAnswersData = (right: Answer[], wrong: Answer[]) => {
+    right.forEach((answer) => {
+      const isNew = !answer.optional;
+      const updateData = updateWord({ isRight: true, answer }, 'sprint');
+      if (updateData.newLearned) {
+        const prevLearned = learned.current;
+        learned.current = prevLearned + 1;
+      }
+      if (!isNew) {
+        dispatch(updateUserWordFromTextbook({
+          userId: user.userId,
+          token: user.token,
+          wordId: answer.id,
+          userWord: { ...updateData.newStatistic },
+        }));
+      } else {
+        dispatch(createUserWordFromTextbook({
+          userId: user.userId,
+          token: user.token,
+          wordId: answer.id,
+          userWord: { ...updateData.newStatistic },
+        }));
+      }
+    });
+    wrong.forEach((answer) => {
+      const isNew = !answer.optional;
+      const updateData = updateWord({ isRight: false, answer }, 'sprint');
+      if (!isNew) {
+        dispatch(updateUserWordFromTextbook({
+          userId: user.userId,
+          token: user.token,
+          wordId: answer.id,
+          userWord: { ...updateData.newStatistic },
+        }));
+      } else {
+        dispatch(createUserWordFromTextbook({
+          userId: user.userId,
+          token: user.token,
+          wordId: answer.id,
+          userWord: { ...updateData.newStatistic },
+        }));
+      }
+    });
+  };
+
+  const getNewStatistic = () => {
+    if (!checkDate(statistic.optional.statisticDay, getToday())) {
+      const dayStat = getNewDayStat(statistic);
+      sendDayStatistic(dayStat);
+      dispatch(updateStatistic({
+        userId: user.userId,
+        token: user.token,
+        statistic: initialState.statistic,
+      }));
+    }
+    updateAnswersData(rightAnswers as Answer[], wrongAnswers as Answer[]);
+    const sprintState = statistic.optional.sprint;
+
+    const newStat: IStatistic = {
+      learnedWords: statistic.learnedWords + learned.current,
+      optional: {
+        audiochallenge: {
+          ...statistic.optional.audiochallenge,
+        },
+        sprint: {
+          newWords: countNewWords([...rightAnswers, ...wrongAnswers]) + sprintState.newWords,
+          correctAnswers: rightAnswers.length + sprintState.correctAnswers,
+          wrongAnswers: wrongAnswers.length + sprintState.wrongAnswers,
+          longestCombo: sprintState.longestCombo > maxCombo ? sprintState.longestCombo : maxCombo,
+          gamesPlayed: sprintState.gamesPlayed + 1,
+        },
+        statisticDay: getToday(),
+      },
+    };
+    dispatch(updateStatistic({
+      userId: user.userId,
+      token: user.token,
+      statistic: newStat,
+    }));
+  };
 
   useEffect(() => {
     if (gameTime <= 0 || roundIndex >= words.length) {
+      if (isLogged) {
+        getNewStatistic();
+      }
       setGameFinished(true);
     }
   }, [gameTime, roundIndex]);
@@ -40,8 +175,15 @@ const Sprint: FC = () => {
     }
   }, [roundIndex]);
 
-  useEffect(() => function clearState() {
-    dispatch(clearSprintState());
+  useEffect(() => {
+    if (isLogged) {
+      dispatch(fetchUserSettings(user));
+      dispatch(fetchUserStatistic(user));
+    }
+
+    return function clearState() {
+      dispatch(clearSprintState());
+    };
   }, []);
 
   return (
