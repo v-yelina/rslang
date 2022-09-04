8d45ed5b892f9ea18bb126685ebd718b63bf8b36
Eremor <artem.s13@gmail.com>
2022-09-04 21:24:51 +0300

    feat: change active menu depending on url

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,5 +1,10 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import {
+  Link,
+  Outlet,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
 import {
   Button, Typography, Menu, Layout,
 } from 'antd';
@@ -23,7 +28,8 @@ const Header: FC = () => {
   const { isLogged } = useAppSelector((state) => state.auth);
   const [isShow, setIsShow] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
-  const [selected, setSelected] = useState(`${sessionStorage.getItem('link') || 'home'}`);
+  const [selected, setSelected] = useState('');
+  const location = useLocation();
 
   const updateDimensions = () => {
     const width = window.innerWidth;
@@ -50,19 +56,37 @@ const Header: FC = () => {
     (windowWidth <= 768) ? setIsShow(false) : setIsShow(true)
   ), [windowWidth]);
 
-  const toggleMenuItem = (url: string) => {
+  useEffect(() => {
+    const url = location.pathname.split('/');
+    switch (url[url.length - 1]) {
+      case '':
+        setSelected('home');
+        break;
+      case 'textbook':
+        setSelected('textbook');
+        break;
+      case 'sprint':
+        setSelected('sprint');
+        break;
+      case 'audiochallenge':
+        setSelected('audiochallenge');
+        break;
+      case 'statistics':
+        setSelected('statistics');
+        break;
+      default:
+        setSelected('');
+    }
+
     if (windowWidth <= 768) {
       setIsShow(false);
     }
-    setSelected(url);
-    sessionStorage.setItem('link', url);
-  };
+  }, [location]);
 
   const logout = () => {
     localStorage.removeItem('user');
     dispatch(clearAuth());
     navigate('/');
-    toggleMenuItem('home');
   };
 
   const setSourceAndTypeGame = (source: WordsSourceType, type: GameType): void => {
@@ -77,13 +101,11 @@ const Header: FC = () => {
       key: 'home',
       label: <Link to="/">Home</Link>,
       className: 'header__link',
-      onClick: () => toggleMenuItem('home'),
     },
     {
       key: 'textbook',
       label: <Link to="/textbook">Textbook</Link>,
       className: 'header__link',
-      onClick: () => toggleMenuItem('textbook'),
     },
     {
       key: 'sprint',
@@ -96,7 +118,6 @@ const Header: FC = () => {
         </Link>
       ),
       className: 'header__link',
-      onClick: () => toggleMenuItem('sprint'),
     },
     {
       key: 'audiochallenge',
@@ -109,13 +130,11 @@ const Header: FC = () => {
         </Link>
       ),
       className: 'header__link',
-      onClick: () => toggleMenuItem('audiochallenge'),
     },
     {
       key: 'statistics',
       label: <Link to="/statistics">Statistics</Link>,
       className: 'header__link',
-      onClick: () => toggleMenuItem('statistics'),
     },
   ];
 
@@ -149,7 +168,7 @@ const Header: FC = () => {
                   {
                   isLogged
                     ? <Button type="primary" onClick={logout} className="btn btn--logout">Log out</Button>
-                    : <Link to="/login" className="btn btn--login" onClick={() => toggleMenuItem('')}>Login</Link>
+                    : <Link to="/login" className="btn btn--login">Login</Link>
                 }
                 </div>
               </div>
