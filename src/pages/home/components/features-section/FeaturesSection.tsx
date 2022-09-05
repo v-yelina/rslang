3a47c88fd6cf5adf6606a150f20e8cf98faf0f7b
Nina Sitaeva <sitaeva.nn@gmail.com>
2022-09-05 07:29:24 +0200

    feat: start features section

---
+++ src/pages/home/components/features-section/FeaturesSection.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import './features-section.scss';
+
+const FeaturesSection: FC = () => (
+  <div className="features">
+    <h2 className="features__title">
+      How It
+      {' '}
+      <span>Works?</span>
+    </h2>
+  </div>
+);
+
+export default FeaturesSection;
